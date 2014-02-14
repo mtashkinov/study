42d64039b37d3c13201512309f17f5a5d326adf1
mtashkinov <mikhailtash@gmail.com>
2014-02-14 18:13:18 +0400

    Sum of list elements
    Count sum of list elements

---
+++ sum.fs
@@ -0,0 +1,18 @@
+﻿(* Tashkinov Mikhail (c) 2014
+   Count sum of list elements
+*)
+
+let rec sum list =
+    match list with
+    | [] -> 0
+    | head :: tail -> sum tail + head
+
+let rec scan size =
+    match size with
+    | 0 -> []
+    | _ -> let el = System.Int32.Parse(System.Console.ReadLine())
+           el :: scan (size - 1)
+            
+let size = System.Int32.Parse(System.Console.ReadLine())
+
+printfn "%A" (sum (scan size))
