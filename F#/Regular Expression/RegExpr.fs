a636cb79b9bbf64fbff879f596174988fe32a7a6
Mikhail Tashkinov <mikhail.tashkinov@medm.com>
2014-09-26 00:58:27 +0400

    Regular expression for email

---
+++ F#/Regular Expression/RegExpr.fs
@@ -0,0 +1,10 @@
+﻿(* Tashkinov Mikhail 2014
+   Regular Expression for expression
+*)
+
+module RegExpr
+
+let isEmail s =
+    let reg = "^[a-zA-Z_]([.]?[\w\-\d]){2,30}[a-zA-Z\d][@]([\w\d]{2,}[\.])+(([a-zA-Z]{2,4}|marketing|sales|support|abuse|security|postmaster|hostmaster|usenet|webmaster|museum))$"
+    let x = new System.Text.RegularExpressions.Regex(reg)
+    x.IsMatch(s)
---
+++ F#/Regular Expression/Tests.fs
@@ -0,0 +1,50 @@
+﻿(* Tashkinov Mikhail (c) 2014
+   Tests for email check
+*)
+
+module RegExprTest
+
+open RegExpr
+open NUnit.Framework
+
+[<TestFixture>]
+type Tests() =
+    [<Test>]
+    member x.Test1() =
+        Assert.AreEqual(isEmail "abcd@yandex.ru", true, "Обычный email")
+    [<Test>]
+    member x.Test2() =
+        Assert.AreEqual(isEmail ".abcd@yandex.ru", false, "email не может начинаться с точки")
+    [<Test>]
+    member x.Test3() =
+        Assert.AreEqual(isEmail "_abcd@yandex.ru", true, "email не может начинаться с подчёркивания")
+    [<Test>]
+    member x.Test4() =
+        Assert.AreEqual(isEmail "abc@yandex.ru", false, "Левая часть email не может быть короче 4х символов")
+    [<Test>]
+    member x.Test5() =
+        Assert.AreEqual(isEmail "abc0@yandex.ru", true, "Левая часть email может оканчиваться на цифру ")
+    [<Test>]
+    member x.Test6() =
+        Assert.AreEqual(isEmail "abcdefghijklmnopqrstuvwxyz1234567890@yandex.ru", false, "Левая часть email не может быть слишком длинной(длиннее 32х символов)")
+    [<Test>]
+    member x.Test7() =
+        Assert.AreEqual(isEmail "abcdyandex.ru", false, "email должен содержать @")
+    [<Test>]
+    member x.Test8() =
+        Assert.AreEqual(isEmail "abcd@yandex.spb.petergof.ru", true, "email может содержать более 1 точки после @")
+    [<Test>]
+    member x.Test9() =
+        Assert.AreEqual(isEmail "abcd@yandex.spb.petergof.russia", false, "После последней точкидолжно быть от 2х до 4х символов")
+    [<Test>]
+    member x.Test10() =
+        Assert.AreEqual(isEmail "ab-cd@yandex.spb.petergof.ru", true, "email может содержать -")
+    [<Test>]
+    member x.Test11() =
+        Assert.AreEqual(isEmail "a..b@yandex.spb.petergof.ru", false, "Между точками должно что-то быть")
+    [<Test>]
+    member x.Test12() =
+        Assert.AreEqual(isEmail "paints_department@hermitage.museum", true, "Бывают валидные имена")
+
+
+
