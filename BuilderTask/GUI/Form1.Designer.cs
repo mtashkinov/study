2485838685e9e9fd327d82acd0313bd3be222b01
mtashkinov <mikhailtash@gmail.com>
2014-12-12 20:25:12 +0300

    Builder task

---
+++ BuilderTask/GUI/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(643, 386);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 63);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Generate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(701, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ BuilderTask/GUI/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        private Intersect.Set set1 = null, set2 = null;
+        Random rnd = new System.Random();
+        private const int size = 500;
+        private const int padding = 50;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            set1 = getSet();
+            set2 = getSet();
+            label1.Text = (Intersect.intersect(set1, set2) == Intersect.Set.NoPoint) ? "no" : "yes";
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if ((set1 != null) && (set2 != null))
+            {
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                paintSet(Color.Red, e, set1);
+                paintSet(Color.Green, e, set2);
+            }
+        }
+
+        private Intersect.Set getSet()
+        {
+            int type = rnd.Next(0, 4);
+
+            switch (type)
+            {
+                case 0:
+                    return Intersect.Set.NewPoint(rnd.Next(padding, size - padding), rnd.Next(padding, size - padding));
+                case 1:
+                    return Intersect.Set.NewLine((double)rnd.Next(1, 100)/10, rnd.Next(0, size/2));
+                case 2:
+                    return Intersect.Set.NewVerticalLine(rnd.Next(padding, size - padding));
+                case 3:
+                    return Intersect.Set.NewLineSegment(Tuple.Create<double, double>(rnd.Next(padding, size - padding), rnd.Next(padding, size - padding)),
+                                                        Tuple.Create<double, double>(rnd.Next(padding, size - padding), rnd.Next(padding, size - padding)));
+                default:
+                    return Intersect.Set.NoPoint;
+            }
+        }
+
+        private void paintSet(Color color, PaintEventArgs e, Intersect.Set set)
+        {
+            if (set.IsPoint)
+            {
+                Point point = new Point((int)((Intersect.Set.Point)set).Item1, (int)((Intersect.Set.Point)set).Item2);
+                e.Graphics.DrawEllipse(new Pen(color), new Rectangle((int)((Intersect.Set.Point)set).Item1, (int)((Intersect.Set.Point)set).Item2, 10, 10));
+            }
+
+            if (set.IsLine)
+            {
+                Point point1 = new Point(0, (int)((Intersect.Set.Line)set).Item2);
+                Point point2 = new Point(size, (int)((Intersect.Set.Line)set).Item1 * size + (int)((Intersect.Set.Line)set).Item2);
+                e.Graphics.DrawLine(new Pen(color), point1, point2);
+            }
+
+            if (set.IsVerticalLine)
+            {
+                Point point1 = new Point((int)((Intersect.Set.VerticalLine)set).Item, 0);
+                Point point2 = new Point((int)((Intersect.Set.VerticalLine)set).Item, size);
+                e.Graphics.DrawLine(new Pen(color), point1, point2);
+            }
+
+            if (set.IsLineSegment)
+            {
+                Point point1 = new Point((int)((Intersect.Set.LineSegment)set).Item1.Item1, (int)((Intersect.Set.LineSegment)set).Item1.Item2);
+                Point point2 = new Point((int)((Intersect.Set.LineSegment)set).Item2.Item1, (int)((Intersect.Set.LineSegment)set).Item2.Item2);
+                e.Graphics.DrawLine(new Pen(color), point1, point2);
+            }
+        }
+    }
+}
