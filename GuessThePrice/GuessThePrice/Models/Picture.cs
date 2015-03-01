4fe336ebe2b92cba7bc043b691f7e7b89ddeae50
mtashkinov <mikhailtash@gmail.com>
2015-03-01 22:49:25 +0300

    Guess The Price Site

---
+++ GuessThePrice/GuessThePrice/Models/Picture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuessThePrice.Models
+
+{
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Auth;
+    using Microsoft.WindowsAzure.Storage.Table;
+    using System.Configuration;
+    public class Picture : TableEntity
+    {
+        public Picture(string author, string name)
+        {
+            this.PartitionKey = name;
+            this.RowKey = author;
+        }
+
+        public Picture() { }
+        public long Price { get; set; }
+        public string URL { get; set; }
+    }
+}
