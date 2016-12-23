9591eff73a4d953539fef027367d35e5b9dee005
Mikhail Tashkinov <mikhail.tashkinov@medm.com>
2016-12-23 09:04:42 +0300

    General functions for text box validation

--- DotNetCourse/Films/Films/Helpers/Validator.cs
+++ DotNetCourse/Films/Films/Helpers/Validator.cs
@@ -11,133 +11,32 @@ namespace Films
     {
         public static bool IsNameValidForSearch(string text, out string errorMessage)
         {
-            if (text == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            bool res = new Regex(@"^[\*?]*[а-яА-Я0-9-!+\.\s]*[\*?]*$").IsMatch(text);
-            if (!res)
-            {
-                errorMessage = Properties.Resources.InvalidChars;
-            } else
-            {
-                errorMessage = "";
-            }
-
-            return res;
+            return IsValidForSearch(text, new Regex(@"^[\*?]*[а-яА-Я0-9-!+\.\s]*[\*?]*$"), out errorMessage);
         }
 
         public static bool IsHumanValidForSearch(string text, out string errorMessage)
         {
-            if (text == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            bool res = new Regex(@"^[\*?]*[а-яА-Я\s-\.]*[\*?]*$").IsMatch(text);
-            if (!res)
-            {
-                errorMessage = Properties.Resources.InvalidChars;
-            }
-            else
-            {
-                errorMessage = "";
-            }
-
-            return res;
+            return IsValidForSearch(text, new Regex(@"^[\*?]*[а-яА-Я\s-\.]*[\*?]*$"), out errorMessage);
         }
 
         public static bool IsCountryValidForSearch(string text, out string errorMessage)
         {
-            if (text == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            bool res = new Regex(@"^[\*?]*[а-яА-Я-]*[\*?]*$").IsMatch(text);
-            if (!res)
-            {
-                errorMessage = Properties.Resources.InvalidChars;
-            }
-            else
-            {
-                errorMessage = "";
-            }
-
-            return res;
+            return IsValidForSearch(text, new Regex(@"^[\*?]*[а-яА-Я-]*[\*?]*$"), out errorMessage);
         }
 
         public static bool IsYearValidForSearch(string text, out string errorMessage)
         {
-            if (text == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            bool res = new Regex(@"^[\*?]*[\d]*[\*?]*$").IsMatch(text);
-            if (!res)
-            {
-                errorMessage = Properties.Resources.InvalidChars;
-            }
-            else
-            {
-                errorMessage = "";
-            }
-
-            return res;
+            return IsValidForSearch(text, new Regex(@"^[\*?]*[\d]*[\*?]*$"), out errorMessage);
         }
 
         public static bool IsNameValid(string text, out string errorMessage)
         {
-            if (text == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            if (text.Length == 0)
-            {
-                errorMessage = Properties.Resources.FieldEmpty;
-                return false;
-            }
-
-            bool res = new Regex(@"^[а-яА-Я0-9-!+\.\s]*$").IsMatch(text);
-            if (!res)
-            {
-                errorMessage = Properties.Resources.InvalidChars;
-            }
-            else
-            {
-                errorMessage = "";
-            }
-
-            return res;
+            return IsValid(text, new Regex(@"^[а-яА-Я0-9-!+\.\s]*$"), out errorMessage);
         }
 
         public static bool IsCountryValid(string text, out string errorMessage)
         {
-            if (text == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            if (text.Length == 0)
-            {
-                errorMessage = Properties.Resources.FieldEmpty;
-                return false;
-            }
-
-            bool res = new Regex(@"^[а-яА-Я-]*$").IsMatch(text);
-            if (!res)
-            {
-                errorMessage = Properties.Resources.InvalidChars;
-            }
-            else
-            {
-                errorMessage = "";
-            }
-
-            return res;
+            return IsValid(text, new Regex(@"^[а-яА-Я-]*$"), out errorMessage);
         }
 
         public static bool IsYearValid(string text, out string errorMessage)
@@ -170,6 +69,52 @@ namespace Films
                 errorMessage = Properties.Resources.InvalidYear;
                 return false;
             }
+        }
+
+        private static bool IsValidForSearch(String text, Regex regex, out String errorMessage)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            bool res = regex.IsMatch(text);
+            if (!res)
+            {
+                errorMessage = Properties.Resources.InvalidChars;
+            }
+            else
+            {
+                errorMessage = "";
+            }
+
+            return res;
+        }
+
+        private static bool IsValid(String text, Regex regex, out String errorMessage)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (text.Length == 0)
+            {
+                errorMessage = Properties.Resources.FieldEmpty;
+                return false;
+            }
+
+            bool res = regex.IsMatch(text);
+            if (!res)
+            {
+                errorMessage = Properties.Resources.InvalidChars;
+            }
+            else
+            {
+                errorMessage = "";
+            }
+
+            return res;
         }
     }
 }
